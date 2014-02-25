b71015d6f7e0b19eb10912aad66ec4be4f41d154
sackworks <sackworks@gmail.com>
2014-02-24 23:31:49 -0500

    so much fun to share the sreejith

--- js/badukoos.js
+++ js/badukoos.js
@@ -1,3 +1,6 @@
+var Popup = function(url, size, w) {
+	return w.open(url, 'popup', "width=500,menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes"); 
+};
 var Badukoos = (function(w, d) {
 	
 	var triggies = [],
@@ -7,9 +10,7 @@ var Badukoos = (function(w, d) {
     		$(e.data).css({left:e.pageX, top:e.pageY});			
 		};		
 	
-	/**
-		
-	**/
+	
 	$("#sreej").on('click', function(e){
 		$(e.target).css("transform", "translateX(10)");
 	});
@@ -34,5 +35,20 @@ var Badukoos = (function(w, d) {
 			}
 		});
 	});
-		
+	
+	$(".social a").each(function(i, link) {
+		$(link).on("click", function(e) {
+			
+			e.preventDefault();
+			w.open(link.href, 'popup', "width=500,height=500,scrollbars=yes,status=yes"); 
+			if(link.href.indexOf("twitter") > 0) {						
+				TwitterPopup = Popup(link.href, null, w);
+			}
+			if(link.href.indexOf("facebook") > 0) {									
+				FacebookPopup = Popup(link.href+"?u="+"http%3A%2F%2Fwww.badukoos.com", null, w);
+			}
+		});
+	});
+	
+
 })(window, document);
