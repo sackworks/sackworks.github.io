66b945d608b85e75272ba5e0cfb2a785d15586bd
sackworks <sackworks@gmail.com>
2014-02-25 22:29:54 -0500

    tidying up functions into some private facades, make the sreejith bobble on click

--- js/badukoos.js
+++ js/badukoos.js
@@ -2,56 +2,68 @@ var Popup = function(url, size, w) {
 	return w.open(url, 'popup', "width=500,height=500,menubar=yes,toolbar=no,resizable=yes,scrollbars=yes,status=yes"); 
 };
 var Badukoos = (function(w, d) {
-	
-	var triggies = [],
-		sreejeeGoodies = ["#moe", "#devil", "#smooth"],
-		moveGoodie = function(e){
-			$(e.data).css("margin", 0);								
-    		$(e.data).css({left:e.pageX, top:e.pageY});			
-		};		
-	
-	
-	$("#sreej").on('click', function(e){
-		$(e.target).css("transform", "translateX(10)");
-	});
-
-	$(sreejeeGoodies).each(function(i, thing){		
-		triggies.push(false);
-
-		$(thing).on("click", function(e) {
-			if(!triggies[i]) {
-				$(d).bind('mousemove', thing, moveGoodie);
-			} else {					
-				$(d).unbind('mousemove', moveGoodie);
-			}			
-			triggies[i] = !triggies[i];
+	//private methods, members
+	var init = function () {
+		setListeners();
+		bobbleHead();
+	},
+	triggies = [],
+	sreejeeGoodies = ["#moe", "#devil", "#smooth"],
+	moveGoodie = function(e){
+		$(e.data).css("margin", 0);								
+		$(e.data).css({left:e.pageX, top:e.pageY});			
+	};
+	/**
+		Shake the sreejee
+	**/			
+	var bobbleHead = function() {
+		$("#sreej").on('mousedown', function(e){
+			$(e.target).removeClass("off").addClass("on");
 		});
-	});
-	
-	$(w).on("resize", function() {	
-		$(sreejeeGoodies).each(function(i, thing){
-			if($(thing).attr("style") != "") {
-				$(thing).removeAttr("style");
-			}
+		$("#sreej").on('mouseup', function(e){
+			$(e.target).removeClass("on").addClass("off");
+			setTimeout(function() {$(e.target).removeClass("off").removeClass("on");}, 100);
+		});			
+	};
+	/**
+		Attach all behaviors for moes
+	**/	
+	var setListeners = function() {
+		$(".social a").each(function(i, link) {
+			$(link).on("click", function(e) {				
+				e.preventDefault();				
+				if(link.href.indexOf("twitter") > 0) {						
+					TwitterPopup = Popup(link.href, null, w);
+				}
+				if(link.href.indexOf("facebook") > 0) {									
+					FacebookPopup = Popup(link.href+"?u="+"http%3A%2F%2Fwww.badukoos.com", null, w);
+				}
+				if(link.href.indexOf("plus") > 0) {
+					GPlus = Popup(link.href+"?u="+"http%3A%2F%2Fwww.badukoos.com", null, w);	
+				}
+			});
 		});
-	});
-	
-	$(".social a").each(function(i, link) {
-		$(link).on("click", function(e) {
-			
-			e.preventDefault();
-			
-			if(link.href.indexOf("twitter") > 0) {						
-				TwitterPopup = Popup(link.href, null, w);
-			}
-			if(link.href.indexOf("facebook") > 0) {									
-				FacebookPopup = Popup(link.href+"?u="+"http%3A%2F%2Fwww.badukoos.com", null, w);
-			}
-			if(link.href.indexOf("plus") > 0) {
-				GPlus = Popup(link.href+"?u="+"http%3A%2F%2Fwww.badukoos.com", null, w);	
-			}
+		$(sreejeeGoodies).each(function(i, thing){		
+			triggies.push(false);
+			$(thing).on("click", function(e) {
+				if(!triggies[i]) {
+					$(d).bind('mousemove', thing, moveGoodie);
+				} else {					
+					$(d).unbind('mousemove', moveGoodie);
+				}			
+				triggies[i] = !triggies[i];
+			});
 		});
-	});
-	
-
+		$(w).on("resize", function() {	
+			$(sreejeeGoodies).each(function(i, thing){
+				if($(thing).attr("style") != "") {
+					$(thing).removeAttr("style");
+				}
+			});
+		});
+	};
+	return {
+		init : init
+	}	
 })(window, document);
+Badukoos.init();
